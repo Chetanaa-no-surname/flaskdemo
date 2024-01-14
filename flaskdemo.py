a7651b9db5e603f47a70c17119abe14cbba11fe3
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2024-01-14 18:52:10 +0800

    Connected to about us html

--- flaskdemo.py
+++ flaskdemo.py
@@ -13,7 +13,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return "I am still working on this"
+    return render_template("about.html")
 
 
 @app.route('/search', methods=['POST', 'GET'])
@@ -30,10 +30,6 @@ def results():
     page = get_page(search_term)
     return render_template("results.html", page=page)
 
-
-@app.route('/about')
-def about():
-    return render_template("about.html")
 
 
 def get_page(search_term):
